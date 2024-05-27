e3dd8ca58b17872a733f255586bf868a9f489711
Bahram <bahramemami1384@gmail.com>
2024-05-27 23:56:43 +0330

    dao

---
+++ src/main/java/com/Ap_project/DAO/DAO.java
@@ -0,0 +1,20 @@
+package com.Ap_project.DAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface DAO<T> {
+
+    T get(int ID) throws SQLException;
+
+    List<T> getAll() throws SQLException;
+
+    int save(T t) throws SQLException;
+
+    int insert(T t) throws SQLException;
+
+    int update(T t) throws SQLException;
+
+    int delete(T t) throws SQLException;
+
+}
---
+++ src/main/java/com/Ap_project/DAO/UserDAO.java
@@ -0,0 +1,36 @@
+package com.Ap_project.DAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserDAO implements DAO{
+    @Override
+    public Object get(int ID) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List getAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public int save(Object o) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int insert(Object o) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int update(Object o) throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object o) throws SQLException {
+        return 0;
+    }
+}
