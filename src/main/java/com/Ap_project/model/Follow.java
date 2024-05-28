14c2f3a1e90eac8aac2c7ab9bfff3a56a1bee047
Bahram <bahramemami1384@gmail.com>
2024-05-28 23:26:34 +0330

    FollowDAO added and few changes in flields name in Follow Class

--- src/main/java/com/Ap_project/model/Follow.java
+++ src/main/java/com/Ap_project/model/Follow.java
@@ -1,14 +1,30 @@
 package com.Ap_project.model;
 
 public class Follow {
-    private String followerID;
-    private String followedID;
+    private String follower;
+    private String followed;
 
     public Follow(String followerID, String followedID) {
-        this.followerID = followerID;
-        this.followedID = followedID;
+        this.follower = followerID;
+        this.followed = followedID;
     }
 
-    public Follow() {
+    public Follow(){
+    }
+
+    public String getFollower() {
+        return follower;
+    }
+
+    public void setFollower(String follower) {
+        this.follower = follower;
+    }
+
+    public String getFollowed() {
+        return followed;
+    }
+
+    public void setFollowed(String followed) {
+        this.followed = followed;
     }
 }
