767dd36479401a2b7c9f80a7cb46493cee40a1de
Ilia Asadi <iliya.13.asadi@gmail.com>
2024-06-04 17:33:23 +0330

    handler

--- src/main/java/com/Ap_project/DAO/DataBase.java
+++ src/main/java/com/Ap_project/DAO/DataBase.java
@@ -5,16 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBase {
-    private final static String url = "jdbc:mysql://170.0.0.1:3386/MyLinkedIn";
+    private final static String url = "jdbc:mysql://localhost:3306/linked-in";
     private final static String user = "root";
     private final static String password = "Il12345678";
 
+    private static Connection connection;
     private DataBase(){
     }
      public static Connection getConnection() throws SQLException{
-        Connection connection = null;
-        connection = DriverManager.getConnection(url , user , password);
-        return connection;
+        if (connection == null) {
+            connection = DriverManager.getConnection(url, user, password);
+            return connection;
+        } else return connection;
      }
 
 
